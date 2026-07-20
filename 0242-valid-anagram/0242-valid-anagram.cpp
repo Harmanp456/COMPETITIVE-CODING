0bc2ded4b0a2bbe9ba76ed75e40bef84e6f93e2b
HARMANPREET KAUR <iharmansaini51@gmail.com>
2026-07-21 00:19:42 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,10 +4,20 @@ public:
         if(s.length()!=t.length()){
             return false;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        // ----- SORTING---- //
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
 
-        return s==t;
+        // return s==t;
 
+        // ----- HASHMAP -----//
+        int n= s.length();
+        unordered_map<char,int> st1;
+        unordered_map<char,int> st2;
+        for(int i=0;i<n;i++){
+         st1[s[i]]++;
+            st2[t[i]]++;
+        }
+        return st1 == st2;
     }
 };
